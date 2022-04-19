f18f1d02760582b2e25eda16ea415512dc0f2c78
xumingmonica101 <ming.xu@chicagobooth.edu>
2022-04-19 22:39:31 +0000

    HW3-1-Final version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,40 @@
 class ApplicationController < ActionController::Base
   layout(false)
 
+def homepage
+  render({ :template=> "game_templates/rules.html.erb"})
+
+end
+
+def play_paper
+  @comp_move= ["rock","paper","scissors"].sample
+   
+  if @comp_move== "rock"
+  @outcome= "won"
+  elsif @comp_move=="paper"
+  @outcome= "tied"
+  elsif @comp_move=="scissors"
+  @outcome="lost"
+  end
+  
+  render({ :template=> "game_templates/user_paper.html.erb", :layout=>"wrapper.html.erb"})
+end
+
+
+def play_scissors
+  
+  @comp_move= ["rock","paper","scissors"].sample
+   
+  if @comp_move== "rock"
+  @outcome= "lost"
+  elsif @comp_move=="paper"
+  @outcome= "won"
+  elsif @comp_move=="scissors"
+  @outcome="tied"
+  end
+  render({ :template=> "game_templates/user_scissors.html.erb", :layout=>"wrapper.html.erb"})
+end
+
   # Add your actions below this line
   # ================================
 def play_rock
@@ -10,9 +44,17 @@ def play_rock
   #render({ :plain=> "Howdy, world"})
   #render({ :plain=> rand(100)})
   #render({ :html=> "<h1>Hello, world!</h1>".html_safe})
-  render({ :template=> "game_templates/user_rock.html.erb"})
-
-
+  @comp_move= ["rock","paper","scissors"].sample
+   
+  if @comp_move== "rock"
+  @outcome= "tied"
+  elsif @comp_move=="paper"
+  @outcome= "lost"
+  elsif @comp_move=="scissors"
+  @outcome="won"
+  end
+  
+  render({ :template=> "game_templates/user_rock.html.erb", :layout=>"wrapper.html.erb"})
 
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-get("/rock",{ :controller=>"application",
+
+get("/",{ :controller=>"application",:action=>"homepage"})
+
+get("/paper",{ :controller=>"application",:action=>"play_paper"})
+
+get("/scissors",{ :controller=>"application",:action=>"play_scissors"})
+
+  get("/rock",{ :controller=>"application",
  :action=>"play_rock"})
 
 end
