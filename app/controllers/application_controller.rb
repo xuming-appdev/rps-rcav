d4a51ce1dbef1ca2d54a10bb8116ffcfa707d9e2
xumingmonica101 <ming.xu@chicagobooth.edu>
2022-04-19 21:20:17 +0000

    rock pagre V1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,16 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def play_rock
 
+  #redirect_to("http://www.wikipedia.org")
+  
+  #render({ :plain=> "Howdy, world"})
+  #render({ :plain=> rand(100)})
+  #render({ :html=> "<h1>Hello, world!</h1>".html_safe})
+  render({ :template=> "game_templates/user_rock.html.erb"})
+
+
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
+get("/rock",{ :controller=>"application",
+ :action=>"play_rock"})
 
 end
